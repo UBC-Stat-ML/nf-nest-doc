50bdafbb831046e18f59e7344689e6f832b95770
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2025-04-28 22:23:45 -0700

    Another fix in render.sh

--- render.sh
+++ render.sh
@@ -1,5 +1,6 @@
 module load gcc/9.4.0
 module load r/4.4.0
+module load apptainer/1.3.1
 
 # Julia 1.11 is causing issues with RCall: https://github.com/JuliaInterop/JuliaCall/pull/237
 export PATH=~/bin/julia-1.10.8/bin:$PATH
