461c0fa35ccef6489cd0aeafbb2e1bb4251d37d7
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2025-04-28 21:39:44 -0700

    Fix in render scripts

--- render.sh
+++ render.sh
@@ -1,3 +1,11 @@
+module load gcc/9.4.0
+module load r/4.4.0
+
+# Julia 1.11 is causing issues with RCall: https://github.com/JuliaInterop/JuliaCall/pull/237
+export PATH=~/bin/julia-1.10.8/bin:$PATH
+
+julia -v
+
 rm -rf experiment_repo
 rm -rf *_cache
 rm -rf _site 
